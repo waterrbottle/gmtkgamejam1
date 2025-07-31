a8fe5242849baba430f3d321aa6b13d1666b36e5
waterrbottle <112860891+waterrbottle@users.noreply.github.com>
2025-07-31 16:39:24 +0200

    hello

---
+++ gmtkgamejam1/scenes/old_concept/character_body_2d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+const JUMP_VELOCITY = -400.0
+var seq = []
+var index = 0
+func _physics_process(delta: float) -> void:
+	
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity()/4 * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	if index > get_parent().seq.size()-1:
+		index = 0
+	if get_parent().seq[index] == "up":
+		velocity.y = -800
+	if get_parent().seq[index]== "left":
+		velocity.x = -2000
+	if get_parent().seq[ index] == "right":
+		velocity.x = 2000
+	print(index)
+	index += 1
+	
---
+++ gmtkgamejam1/scenes/old_concept/character_body_2d2.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+const JUMP_VELOCITY = -900.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	var push_force = 30.0
+
+	# after calling move_and_slide()
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
